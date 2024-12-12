988f2235c8564c08fdca3734c6a8b799b57a5959
nicholas899 <100855505+nicholas899@users.noreply.github.com>
2024-12-11 23:46:12 -0500

    Modified tests

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -29,9 +29,6 @@ def test_lennard_jones_initialization():
     object can be created with valid parameters."""
     epsilon = 1.0
     sigma = 1.0
-    lj_potential = LennardJonesPotential(epsilon)
+    lj_potential = LennardJonesPotential(epsilon, sigma)
     assert isinstance(lj_potential, LennardJonesPotential)
-    assert lj_potential.epsilon == epsilon
-    if hasattr(lj_potential, "sigma"):
-        lj_potential.sigma = sigma
-        assert lj_potential.sigma == sigma
+    assert lj_potential.compute_force(1.0) is not None
