083b158d880d23e3fdb44f6df48c4be8800fbc99
nicholas899 <100855505+nicholas899@users.noreply.github.com>
2024-12-11 22:31:34 -0500

    Modified tests

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,8 +1,15 @@
-import pytest
+# import pytest
 from script import Atom
+
 
 # Basic test case for Atom initialization
 def test_atom_basic_initialization():
-    """Test if Atom object can be created."""
-    atom = Atom()
+    """Test if Atom object can be created with valid inputs."""
+    position = [0.0, 0.0, 0.0]
+    velocity = [0.0, 0.0, 0.0]
+    mass = 1.0
+    epsilon = 1.0
+    sigma = 1.0
+
+    atom = Atom(position, velocity, mass, epsilon, sigma)
     assert isinstance(atom, Atom)  # Verify the instance type
